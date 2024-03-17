22f2f3ea84e34ebfb1a468349c94cc93f833baa1
nikhilkumar378 <nikhilkumar87378@gmail.com>
2024-03-17 15:26:34 +0530

    setup pf axios instance

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
 //components
-import { Provider } from 'react-redux'
-import App from './App.jsx'
+import { Provider } from "react-redux";
+import App from "./App.jsx";
 
 //css
-import './index.css'
+import "./index.css";
 
 //Library imports
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import store from './Redux/store.js'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import store from "./Redux/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render( 
-    <Provider store={store}>
-    <BrowserRouter> 
-    <App /> 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
-    </Provider>
-)
+  </Provider>
+);
